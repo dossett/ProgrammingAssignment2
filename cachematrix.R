674f14ddb1b2f46f85e883261beb7a9741d33518
Aaron Dossett <dossett@gmail.com>
2014-05-09 21:29:12 -0500

    First commit to the cloned repo

--- cachematrix.R
+++ cachematrix.R
@@ -8,7 +8,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This is Aaron's version of the file
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
